c9407b00f8ec85ff6a19ba4bfe01b0dc565bc199
Brad Chao <brad@brad.tw>
2016-11-25 10:26:45 +0800

    v5

--- app/src/main/java/com/example/user/brad17/MainActivity.java
+++ app/src/main/java/com/example/user/brad17/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.user.brad17;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
 
         webView = (WebView)findViewById(R.id.webview);
         initWebView();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    123);
+
+        }
+
     }
 
     private void initWebView(){
