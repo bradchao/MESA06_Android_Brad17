00067d10cba059003c4b6e7ff7cf04fa444002d5
Brad Chao <brad@brad.tw>
2016-11-25 10:17:36 +0800

    v4

--- app/src/main/java/com/example/user/brad17/MainActivity.java
+++ app/src/main/java/com/example/user/brad17/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.brad17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,5 +23,8 @@ public class MainActivity extends AppCompatActivity {
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/map.html");
+    }
+    public void gotoWhere(View v){
+        webView.loadUrl("javascript:goto(60.464118, 22.209453)");
     }
 }
