2ecabec31f41b57af6cbed71fb614721a2916b62
Brad Chao <brad@brad.tw>
2016-11-25 09:42:47 +0800

    v1

---
+++ app/src/main/java/com/example/user/brad17/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.user.brad17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webview);
+        initWebView();
+    }
+
+    private void initWebView(){
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/map.html");
+    }
+}
